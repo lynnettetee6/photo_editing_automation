51b52770c226dbe09c318bebf18f19c3321f2bc6
lynnettetee <43146875+lynnettetee@users.noreply.github.com>
2025-06-11 20:05:17 +0800

    Instead of using lr plugin, run a script instead
    Ref:https://github.com/shuangye/lightroom-auto-develop/tree/main

---
+++ lr_auto_exposure.lua
@@ -0,0 +1,115 @@
+local LrTasks = import "LrTasks"
+local catalog = import "LrApplication".activeCatalog()
+local ProgressScope = import "LrProgressScope"
+local LrDialogs = import "LrDialogs"
+local LrLogger = import "LrLogger"
+
+local scriptName = "Auto Develop"
+
+-- Follow Lightroom Classic SDK Guide to see the logs
+local myLogger = LrLogger("libraryLogger")  -- log file name; will be in ~/Documents/LrClassicLogs/
+-- myLogger:enable("logfile")
+-- myLogger:enable("print")
+
+
+function mylog(content)
+    local label = scriptName
+    -- print("[" .. label .. "] " .. content)
+    myLogger:trace("[" .. label .. "] " .. content)
+end
+
+
+function setDevelopSettings(settings, key, value)
+    local changed = false
+    local original = settings[key]
+    if original ~= value then
+        settings[key] = value
+        changed = true
+    end
+    return changed
+end
+
+
+-- Handles one photo
+function processPhoto(photo)
+    local fileName = photo:getFormattedMetadata("fileName")
+    local format = photo:getRawMetadata("fileFormat")
+    if format == "VIDEO" then
+        return
+    end
+
+    local iso = photo:getRawMetadata("isoSpeedRating")  -- a number
+    if iso == nil then
+        mylog(fileName .. ": unable to get ISO speed")
+        return
+    end
+
+    local changed = false
+    local developSettings = photo:getDevelopSettings()  -- a table
+    local factor = 33    -- ISO <= 100: Luminance NR = 5; ISO >= 1000: Luminance NR = 30
+
+    -- Luminance noise always exists. So limit the min value.
+    -- Luminance noise reduction becomes less effective when > 30
+    local luminanceNoiseReductionMin, luminanceNoiseReductionMax = 5, 30
+
+    -- Convert to integer to prevent fake changed value in `setDevelopSettings()`
+    -- Seems Lightroom internal Lua interpreter does not support `//` operator: unexpected symbol near '/'
+    local luminanceNoiseReduction = math.floor(iso / factor)
+
+    if luminanceNoiseReduction < luminanceNoiseReductionMin then
+        luminanceNoiseReduction = luminanceNoiseReductionMin
+    end
+    if luminanceNoiseReduction > luminanceNoiseReductionMax then
+        luminanceNoiseReduction = luminanceNoiseReductionMax
+    end
+
+    local colorNoiseReduction = developSettings["ColorNoiseReduction"]  -- number
+    if colorNoiseReduction < luminanceNoiseReduction then
+        colorNoiseReduction = luminanceNoiseReduction
+    end
+
+    mylog(fileName .. ": ISO = " .. iso .. ", will set luminance NR = " .. luminanceNoiseReduction .. ", color NR = " .. colorNoiseReduction)
+
+    local ret = setDevelopSettings(developSettings, "LuminanceSmoothing", luminanceNoiseReduction)
+    changed = changed or ret
+    ret = setDevelopSettings(developSettings, "ColorNoiseReduction", colorNoiseReduction)
+    changed = changed or ret
+    if changed then
+        catalog:withWriteAccessDo(scriptName, function(context)
+            photo:applyDevelopSettings(developSettings, scriptName, false)
+        end )
+    end
+end
+
+
+LrTasks.startAsyncTask(function()
+    -- If there is a selection, `catalog:getTargetPhotos()` returns the list of selected photos.
+    -- Otherwise, it returns the entire list of photos in the filmstrip.
+    -- Returning the entire list of photos in the filmstrip is not desired, so do nothing if no selection.
+    if catalog:getTargetPhoto() == nil then
+        mylog("No photos selected. Nothing to do.")
+        return
+    end
+
+    local photos = catalog:getTargetPhotos()
+    local count = #photos
+
+    --[[
+    prompt = "Will apply auto develop settings to " .. count .. " photo(s)..."
+    local ret = LrDialogs.confirm(scriptName, prompt)
+    if ret ~= "ok" then
+        return
+    end
+    ]]
+
+    local progressScope = ProgressScope({ title = scriptName, caption = scriptName, })
+
+    for i, photo in ipairs(photos) do
+        processPhoto(photo)
+        -- LrTasks.sleep(2)    -- Simulate a time-consuming operation, to check whether the ProgressScope updates correctly.
+        progressScope:setPortionComplete(i / count)
+        progressScope:setCaption("Processing " .. i .. "/" .. count)
+    end
+
+    progressScope:done()
+end )
